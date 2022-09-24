356a0f52aa1f31a3e97d7a1bcb330cbd1b5e5f1e
katemarkos <105130076+katemarkos@users.noreply.github.com>
2022-09-24 17:17:42 +1200

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -4,8 +4,8 @@ for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
   if (firstLetter === 'j') {
     byeSpeaker(names[i]);
-  }
-  else {
+  } else {
     helloSpeaker(names[i]);
   }
-}})();
+}
+})();
